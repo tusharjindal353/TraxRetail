7338cd91709020bdef117ac1d441ab91e30167e3
Tushar Gupta <tusharjindal353@gmail.com>
2025-04-15 22:58:31 +0530

    completed

---
+++ TraxRetail/TraxStorage/Product+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Product+CoreDataClass.swift
+//  TraxRetail
+//
+//  Created by Tushar Gupta on 14/04/2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Product)
+public class Product: NSManagedObject {
+
+}
